6ea475db69a7e028ffacb037e408286beedf3cd1
Ian Rundle <irrundle03@protonmail.com>
2021-11-18 16:17:59 -0600

    Update servo_test.ino

--- drop-test-code/servo_test/servo_test.ino
+++ drop-test-code/servo_test/servo_test.ino
@@ -1,22 +1,22 @@
 #include <Servo.h>
-
+int bias = -4;
 Servo servo; 
 void setup() 
 {
   while (!Serial);
   Serial.begin(9600);
   servo.attach(9); //attach servo to pin 9
-  servo.write(90); //set servo to default center position
+  servo.write(90+bias); //set servo to default center position
 }
  
 void loop()
 {
-  servo.write(90);
+  servo.write(90+bias);
   delay(2000);
-  servo.write(160);
+  servo.write(160+bias);
   delay(2000);
-  servo.write(90);
+  servo.write(90+bias);
   delay(2000);
-  servo.write(20);
+  servo.write(20+bias);
   delay(2000);
 }
