04f682655381e7e3d6304b99b2c122c00e2ca478
jm142 <Jane.Mo@rice.edu>
2022-02-22 23:30:59 -0600

    write_file.py: saves to new file every one second

--- raspi_reading/lsm9ds1-rjg/examples/write_file.py
+++ raspi_reading/lsm9ds1-rjg/examples/write_file.py
@@ -1,10 +1,23 @@
+from datetime import datetime, timedelta
 class DataSaver:
-	def __init__(self, filename):
-		self.fd = open(filename, "w")
+    def __init__(self, filename):
+        self.filename = filename
+        self.filenum = 1
+        self.openfile()
+        self.now = datetime.now()
 
-	def save_data(self, a, g, m):
-		self.fd.write(f"Acc:{a} Gyr:{g} Mag:{m}\n")
+    def openfile(self):
+        self.fd = open(self.filename+str(self.wnum), "w")
+        self.filenum+=1
+    
+    def save_data(self, a, g, m):
+        if datetime.now() >= self.now + timedelta(seconds = 1):
+            self.now = datetime.now()
+            self.close()
+            self.openfile()
+        self.fd.write(f"Acc:{a} Gyr:{g} Mag:{m}\n")
 
-	def close(self):
-		self.fd.close()
 
+    def close(self):
+        self.fd.close()
+		
