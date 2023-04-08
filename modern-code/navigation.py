e298725e0e39fa97c45e377dc07b66f1a8146e2a
Wisha Wanichwecharungruang <wisha@rice.edu>
2023-04-08 16:35:50 -0500

    get rid of compression because apprently it doesn't help that much

--- modern-code/navigation.py
+++ modern-code/navigation.py
@@ -7,7 +7,6 @@ import time
 import threading
 import math
 from gpiozero import Servo
-import lzma
 import datetime
 
 PI = math.pi
@@ -25,7 +24,7 @@ class GpsPoller(threading.Thread):
 		self.current_lat = 0.0
 		self.current_alt = 0.0
 		self.ready = False
-		self.file = lzma.open('output-{date:%Y-%m-%d_%H:%M:%S}-gps.txt'.format( date=datetime.datetime.now() ), 'w	b')
+		self.file = open('output-{date:%Y-%m-%d_%H:%M:%S}-gps.txt'.format( date=datetime.datetime.now() ), 'w', buffering=512)
 		
 	def get_current_value(self):
 		return (self.current_lat, self.current_lon)
@@ -48,12 +47,9 @@ class GpsPoller(threading.Thread):
 					self.current_lon = self.gps.longitude
 					self.current_alt = self.gps.altitude_m
 					
-					self.file.write(f"t={time.monotonic()}, lat={self.current_lat}, lon={self.current_lon}, alt={self.current_alt}\n".encode('utf-8'))
-					
+					self.file.write(f"t={time.monotonic()}, lat={self.current_lat}, lon={self.current_lon}, alt={self.current_alt}\n")			
+		finally:
 			self.file.close()
-			
-		except StopIteration:
-			pass
 
 MAG_MX_BIAS = 0.45215
 MAG_MX_NORM = 0.20125
@@ -74,7 +70,7 @@ class ImuPoller(threading.Thread):
 		self.start_time = time.monotonic()
 		self.imu = lsm9ds1.make_i2c(1)
 		self.current_heading = 0.0
-		self.file = lzma.open('output-{date:%Y-%m-%d_%H:%M:%S}-imu.txt'.format( date=datetime.datetime.now() ), 'wb')
+		self.file = open('output-{date:%Y-%m-%d_%H:%M:%S}-imu.txt'.format( date=datetime.datetime.now() ), 'w', buffering=4096)
 		
 	def get_current_heading(self):
 		return self.current_heading
@@ -107,12 +103,10 @@ class ImuPoller(threading.Thread):
 
 				if da_temp or da_gyro or da_acc or da_mag:
 					mag = [mx, my, mz]
-					self.file.write(f"t={time.monotonic()}, temp={temp if da_temp else NO_DATA}, acc={acc if da_acc else NO_DATA}, gyro={gyro if da_gyro else NO_DATA}, mag={mag if da_mag else NO_DATA}\n".encode('utf-8'))
+					self.file.write(f"t={time.monotonic()}, temp={temp if da_temp else NO_DATA}, acc={acc if da_acc else NO_DATA}, gyro={gyro if da_gyro else NO_DATA}, mag={mag if da_mag else NO_DATA}\n")
 
+		finally:
 			self.file.close()
-			
-		except StopIteration:
-			pass
 		
 	@staticmethod
 	def mag_to_heading(mx, my, mz):
@@ -182,88 +176,5 @@ while True:
 		servo_l.value = 1.0
 		servo_r.value = 1.0
 	
-'''
-try:
-	gpsp = GpsPoller()
-	gpsp.start()
-	prev_lat = 0
-	prev_lon = 0
-	imu = lsm9ds1.make_i2c(1)
-	while True:
-		(lat, lon) = gpsp.get_current_value()
-		if (lat is None) or (lon is None):
-			continue
-		try:
-			print("polling gps")
-			v_y = lat - prev_lat
-			prev_lat = lat
-			v_x = lon - prev_lon
-			prev_lon = lon
-			
-			v_prime_x = -v_y
-			v_prime_y = v_x
-			
-			r_x = target_lon - lon
-			r_y = target_lat - lat
-			
-			v_prime_dot_r = r_x * v_prime_x + r_y * v_prime_y
-			
-			v_dot_r = r_x * v_x + r_y * v_y
-			
-			v_mag = math.sqrt(v_x**2 + v_y**2)
-			r_mag = math.sqrt(r_x**2 + r_y**2)
-			mag_product = v_mag * r_mag
 
-			if mag_product == 0:
-				continue
-
-			abs_angle = math.acos(v_dot_r / mag_product)
-			if (mag_product == 0 or abs_angle < (15/180 * math.pi)):
-				print("doing nothing")
-				servo_l.mid()
-				servo_r.mid()
-				angle = 0
-			else:
-				if (v_prime_dot_r <= 0):
-					print(f"turning right by {abs_angle * 180 / math.pi}deg")
-					angle = abs_angle
-					servo_r.min()
-					servo_l.mid()
-				elif (v_prime_dot_r > 0):
-					print(f"turning left by {abs_angle * 180 / math.pi}deg")
-					angle = -abs_angle
-					servo_l.min()
-					servo_r.mid()
-
-			target_heading = None
-			while True:
-				mag_data_ready = imu.read_magnetometer_status().data_available
-				if mag_data_ready:
-					mx, my, mz = imu.mag_values()
-					mx = (mx + 0.7366) * 4.26167
-					mz = (mz + 0.3655) * 5.58036
-					heading = mag_to_heading(mx, my, mz)
-					if target_heading is None:
-						target_heading = (heading + angle) % (2 * math.pi)
-					else:
-						heading_difference = abs(target_heading - heading)
-						heading_difference = min(heading_difference, (2*math.pi) - heading_difference)
-						print(f"still turning - heading difference = {heading_difference * 180 / math.pi}deg")
-						if heading_difference < (15/180 * math.pi):
-							servo_l.mid()
-							servo_r.mid()
-							break
-				time.sleep(0.1)
-			print("turn complete")
-			time.sleep(2.0)
-			time.sleep(0.25)
-		except(AttributeError, KeyError):
-			pass
-		time.sleep(0.25)
-
-except(KeyboardInterrupt, SystemExit):
-	print("\nKilling Thread..")
-	gpsp.running = False
-	gpsp.join()
-'''
 print("Done.\nExiting.")
